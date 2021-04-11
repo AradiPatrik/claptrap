70d9dabd1dd26a55eb200e1f74e1f8ac9bbac4a3
EmilFekete <fekete.feketeemil@gmail.com>
2021-04-11 16:29:56 +0200

    add db connection

--- modules/backend-spring/build.gradle.kts
+++ modules/backend-spring/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.springframework.boot").version("2.3.3.RELEASE")
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.springframework.boot").version("2.4.3")
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 tasks.withType<Jar> {
@@ -19,6 +19,10 @@ dependencies {
     implementation(project(":core:domain-models"))
     implementation(project(":core:domain-network-mappers"))
     implementation(project(":core:json-adapters"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("io.r2dbc:r2dbc-postgresql")
+    implementation("org.postgresql:postgresql")
 
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-security")
