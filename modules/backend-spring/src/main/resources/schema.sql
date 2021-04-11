1e18a912d300d3e5da58f936abe93474b7c181fe
EmilFekete <fekete.feketeemil@gmail.com>
2021-04-11 17:20:39 +0200

    ssl

--- modules/backend-spring/src/main/resources/schema.sql
+++ modules/backend-spring/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE customer;
+DROP TABLE IF EXISTS customer;
 CREATE TABLE customer (id SERIAL PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255));
 
 INSERT INTO customer (id, first_name, last_name) values (0, 'Patrik', 'Aradi');
