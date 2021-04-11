bf7b99085267a7f237cc809736182bfd3f146e28
EmilFekete <fekete.feketeemil@gmail.com>
2021-04-11 17:07:34 +0200

    ssl

--- modules/backend-spring/src/main/java/emilfekete/claptrap/backend/db/DatabaseConfiguration.java
+++ modules/backend-spring/src/main/java/emilfekete/claptrap/backend/db/DatabaseConfiguration.java
@@ -1,10 +1,13 @@
 package emilfekete.claptrap.backend.db;
 
+import emilfekete.ReactiveDemoApplication;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.postgresql.client.SSLMode;
 import io.r2dbc.spi.ConnectionFactory;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
@@ -17,24 +20,31 @@ import java.net.URISyntaxException;
 @EnableR2dbcRepositories
 class DatabaseConfiguration extends AbstractR2dbcConfiguration {
 
+  private static final Logger log = LoggerFactory.getLogger(ReactiveDemoApplication.class);
+
   @Bean
   public ConnectionFactory connectionFactory() {
     URI dbUri = getDatabaseUri();
 
     String username = dbUri.getUserInfo().split(":")[0];
     String password = dbUri.getUserInfo().split(":")[1];
+    log.info(dbUri.toASCIIString());
 
-    return new PostgresqlConnectionFactory(
+    PostgresqlConnectionFactory factory = new PostgresqlConnectionFactory(
       PostgresqlConnectionConfiguration.builder()
         .host(dbUri.getHost())
         .port(dbUri.getPort())
         .database(dbUri.getPath().substring(1))
         .username(username)
         .password(password)
-        .sslMode( SSLMode.REQUIRE )
         .enableSsl()
+        .sslMode(SSLMode.REQUIRE)
         .build()
     );
+
+    log.info(factory.toString());
+
+    return factory;
   }
 
   @NotNull
