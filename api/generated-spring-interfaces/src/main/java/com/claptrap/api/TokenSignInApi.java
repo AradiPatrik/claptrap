39d7abe7fbf8e8c60bf9b48cd86c8c6fadbee102
EmilFekete <fekete.feketeemil@gmail.com>
2021-05-01 18:51:53 +0200

    get board with formatting

--- api/generated-spring-interfaces/src/main/java/com/claptrap/api/TokenSignInApi.java
+++ api/generated-spring-interfaces/src/main/java/com/claptrap/api/TokenSignInApi.java
@@ -22,28 +22,28 @@ import springfox.documentation.annotations.ApiIgnore;
 import java.util.List;
 import java.util.Map;
     import java.util.Optional;
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-05-01T18:46:15.460440100+02:00[Europe/Budapest]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-05-01T18:51:00.697273700+02:00[Europe/Budapest]")
 @Api(value = "token-sign-in", description = "the token-sign-in API")
     public interface TokenSignInApi {
 
-            /**
-            * PUT /token-sign-in : Validates the token, and initializes the user identified by the sub claim if the user is not already present inside the database. This token can come from either the Google, or the Firebase sign in provider. 
-            *
-            * @return Validation successful. User was already present in the database. (status code 200)
-                *         or Validation successful. User was created. (status code 201)
-                *         or Token validation failed. The token is either faulty or expired. (status code 401)
-            */
-            @ApiOperation(value = "Validates the token, and initializes the user identified by the sub claim if the user is not already present inside the database. This token can come from either the Google, or the Firebase sign in provider. ", nickname = "tokenSignIn", notes = "", response = DummyWire.class, authorizations = {
+    /**
+        * PUT /token-sign-in : Validates the token, and initializes the user identified by the sub claim if the user is not already present inside the database. This token can come from either the Google, or the Firebase sign in provider. 
+        *
+        * @return Validation successful. User was already present in the database. (status code 200)
+        *         or Validation successful. User was created. (status code 201)
+        *         or Token validation failed. The token is either faulty or expired. (status code 401)
+    */
+    @ApiOperation(value = "Validates the token, and initializes the user identified by the sub claim if the user is not already present inside the database. This token can come from either the Google, or the Firebase sign in provider. ", nickname = "tokenSignIn", notes = "", response = DummyWire.class, authorizations = {
         
-        @Authorization(value = "bearerAuth")
+    @Authorization(value = "bearerAuth")
          }, tags={  })
-            @ApiResponses(value = { 
-                @ApiResponse(code = 200, message = "Validation successful. User was already present in the database.", response = DummyWire.class),
-                @ApiResponse(code = 201, message = "Validation successful. User was created.", response = DummyWire.class),
-                @ApiResponse(code = 401, message = "Token validation failed. The token is either faulty or expired.") })
-            @PutMapping(
-            value = "/token-sign-in",
-            produces = { "application/json" }
-            )
-        Mono<DummyWire> tokenSignIn(@AuthenticationPrincipal Mono<Jwt> jwt);
-        }
+    @ApiResponses(value = { 
+    @ApiResponse(code = 200, message = "Validation successful. User was already present in the database.", response = DummyWire.class),
+    @ApiResponse(code = 201, message = "Validation successful. User was created.", response = DummyWire.class),
+    @ApiResponse(code = 401, message = "Token validation failed. The token is either faulty or expired.") })
+    @PutMapping(
+        value = "/token-sign-in",
+        produces = { "application/json" }
+    )
+    Mono<DummyWire> tokenSignIn(@AuthenticationPrincipal Mono<Jwt> jwt);
+}
